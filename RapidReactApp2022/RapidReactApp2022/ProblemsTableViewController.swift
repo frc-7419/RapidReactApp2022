bd2c471e29eac4ce583f29ed7571cf28a22cda94
pryme-svg <edoc.www@gmail.com>
2022-01-29 15:32:00 -0800

    Dropdown and Add Cell functionality

--- RapidReactApp2022/RapidReactApp2022/ProblemsTableViewController.swift
+++ RapidReactApp2022/RapidReactApp2022/ProblemsTableViewController.swift
@@ -1,32 +1,73 @@
 import Foundation
 import UIKit
 
+class AddProblemTableViewCell: UITableViewCell {
+    @IBOutlet weak var addProblemButton: UIButton!
+    
+}
+
+class ProblemTableViewCell: UITableViewCell {
+    
+    var gameSection: String?
+    
+    @IBOutlet weak var gameSectionButton: UIButton! {
+        didSet {
+            gameSectionButton.menu = addMenuItems()
+        }
+    }
+    
+    func addMenuItems() -> UIMenu {
+        let menuItems = UIMenu(title: "", options: .displayInline, children: [
+            UIAction(title: "Autonomous", image: UIImage(systemName: "gamecontroller"), handler: { (_) in
+                self.gameSection = "Autonomous"
+            }),
+            UIAction(title: "Tele-Op", image: UIImage(systemName: "gamecontroller"), handler: { (_) in
+                self.gameSection = "Tele-Op"
+            }),
+            UIAction(title: "Endgame", image: UIImage(systemName: "gamecontroller"), handler: { (_) in
+                self.gameSection = "Endgame"
+            })
+        ])
+        return menuItems
+    }
+    
+
+}
+
 class ProblemsTableViewController: UITableViewController {
     
+    var rows = 1
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "problemCell", for: indexPath)
+        var cell: UITableViewCell!
         
+        if (indexPath.section == 0) { // problemCell
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "problemCell", for: indexPath) as! ProblemTableViewCell
+            cell = tableView.dequeueReusableCell(withIdentifier: "problemCell", for: indexPath)
+        }
+        else if (indexPath.section == 1) { // addProblemCell
+            let cell_i = tableView.dequeueReusableCell(withIdentifier: "addProblemCell", for: indexPath) as! AddProblemTableViewCell
+            cell_i.addProblemButton.addTarget(self, action: #selector(addRow), for: .touchUpInside)
+            cell = cell_i
+        }
         return cell
+    }
+    
+    @objc func addRow() {
+        rows += 1
+        self.tableView.reloadData()
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
+
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (section == 0) {
+            return rows
+        }
         return 1
     }
-    
-    // pad cells
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = UIView()
-        headerView.backgroundColor = view.backgroundColor
-        return headerView
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 1
-    }
-
 
 }
