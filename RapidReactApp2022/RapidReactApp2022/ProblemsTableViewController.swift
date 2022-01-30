c7b68243f2ff0d79f4da42badda978aa9b116be6
pryme-svg <edoc.www@gmail.com>
2022-01-30 12:30:14 -0800

    add `.data` property to ProblemTableViewCell

--- RapidReactApp2022/RapidReactApp2022/ProblemsTableViewController.swift
+++ RapidReactApp2022/RapidReactApp2022/ProblemsTableViewController.swift
@@ -8,24 +8,61 @@ class AddProblemTableViewCell: UITableViewCell {
 
 class ProblemTableViewCell: UITableViewCell {
     
-    var gameSection: String?
+    var data: [String: Any] = [
+        "problem": "",
+        "section": "",
+        "enabled": false
+    ]
     
     @IBOutlet weak var gameSectionButton: UIButton! {
         didSet {
             gameSectionButton.menu = addMenuItems()
+            self.data["section"] = gameSectionButton.menu!.children[0].title // set inital section to first element
         }
+    }
+    
+    @IBAction func textFieldChanged(_ sender: UITextField) {
+        let data = sender.text!
+        self.data["problem"] = data
+        // if problem empty disable
+        if (data == "") {
+            problemToggle.setOn(false, animated: true) // off state
+            problemToggle.isEnabled = false // disable
+        }
+        // else enable
+        else if (!problemToggle.isOn && !problemToggle.isEnabled) {
+            problemToggle.setOn(true, animated: true)
+            problemToggle.isEnabled = true
+        }
+        // call manually because not UIEvent
+        switchChanged()
+        
+        print(self.data)
+    }
+    
+    @IBOutlet weak var problemToggle: UISwitch! {
+        // disable initially because problem == ""
+        didSet {
+            problemToggle.setOn(false, animated: true)
+            problemToggle.isEnabled = false
+            problemToggle.addTarget(self, action: #selector(switchChanged), for: UIControl.Event.valueChanged)
+        }
+    }
+    
+    @objc func switchChanged() {
+        self.data["enabled"] = problemToggle.isOn
     }
     
     func addMenuItems() -> UIMenu {
         let menuItems = UIMenu(title: "", options: .displayInline, children: [
             UIAction(title: "Autonomous", image: UIImage(systemName: "gamecontroller"), handler: { (_) in
-                self.gameSection = "Autonomous"
+                self.data["section"] = "Autonomous"
             }),
             UIAction(title: "Tele-Op", image: UIImage(systemName: "gamecontroller"), handler: { (_) in
-                self.gameSection = "Tele-Op"
+                self.data["section"] = "Tele-Op"
             }),
             UIAction(title: "Endgame", image: UIImage(systemName: "gamecontroller"), handler: { (_) in
-                self.gameSection = "Endgame"
+                self.data["section"] = "Endgame"
             })
         ])
         return menuItems
