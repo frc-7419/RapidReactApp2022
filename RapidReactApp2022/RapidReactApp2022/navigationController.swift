d89a915521b52728245392d9308da932e1a73db7
AaravWattal <61303690+AaravWattal@users.noreply.github.com>
2022-02-02 19:56:40 -0800

    tab bar controller WITH ERROR

---
+++ RapidReactApp2022/RapidReactApp2022/navigationController.swift
@@ -0,0 +1,41 @@
+//
+//  navigationController.swift
+//  RapidReactApp2022
+//
+//  Created by Aarav Wattal on 2/2/22.
+
+
+//import SwiftUI
+//
+//struct ContentView: View {
+//  var body: some View {
+//    NavigationView {
+//
+//      TabView {
+//        FeedView()
+//          .tabItem {
+//            Image(systemName: "house")
+//            Text("home")
+//          }
+//        SearchView()
+//          .tabItem {
+//            Image(systemName: "magnifyingglass")
+//            Text("Search")
+//          }
+//        Text("Message")
+//          .tabItem {
+//            Image(systemName: "envelope")
+//            Text("Messages")
+//          }
+//      }
+//      .navigationBarTitle("Home")
+//      .navigationBarTitleDisplayMode(.inline)
+//    }
+//  }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    ContentView()
+//  }
+//}
