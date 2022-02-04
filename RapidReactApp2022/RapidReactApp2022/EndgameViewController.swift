ee6857f526784430278da5f588797890ed6d1ccb
Franklin <>
2022-02-04 10:19:05 -0800

    added another button

--- RapidReactApp2022/RapidReactApp2022/EndgameViewController.swift
+++ RapidReactApp2022/RapidReactApp2022/EndgameViewController.swift
@@ -19,14 +19,24 @@ class EndgameViewController: UIViewController {
     private var hangerBonus: Bool = false
     
     
-    //stepper label
-    @IBOutlet weak var BallsLabel: UILabel!
+    //stepper label for upperhub
+    @IBOutlet weak var UpperLabel: UILabel!
     
     
-    //stepper
-    @IBAction func Stepper(_ sender: UIStepper) {
-        BallsLabel.text = String(sender.value)
+    //stepper for upperhub
+    @IBAction func UpperStepper(_ sender: UIStepper) {
+        UpperLabel.text = String(Int(sender.value))
     }
+    
+    //stepper label for lowerhub
+    @IBOutlet weak var LowerLabel: UILabel!
+
+    
+    //stepper for lowerhub
+    @IBAction func LowerStepper(_ sender: UIStepper) {
+        LowerLabel.text = String(Int(sender.value))
+    }
+    
     
     //segmented control
     @IBAction func SegmentedControl(_ sender: UISegmentedControl) {
---
+++ RapidReactApp2022/RapidReactApp2022/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  RapidReactApp2022
+//
+//  Created by Franklin on 2/2/22.
+//
+
+import Foundation
