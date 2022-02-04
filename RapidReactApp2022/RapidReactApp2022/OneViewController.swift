8862a3a6b6d06862e9fc9ab378985ff746e1d596
AaravWattal <61303690+AaravWattal@users.noreply.github.com>
2022-02-03 17:38:46 -0800

    no message

---
+++ RapidReactApp2022/RapidReactApp2022/OneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OneViewController.swift
+//  RapidReactApp2022
+//
+//  Created by Aarav Wattal on 2/3/22.
+//
+
+import UIKit
+
+class OneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RapidReactApp2022/RapidReactApp2022/TwoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TwoViewController.swift
+//  RapidReactApp2022
+//
+//  Created by Aarav Wattal on 2/3/22.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
