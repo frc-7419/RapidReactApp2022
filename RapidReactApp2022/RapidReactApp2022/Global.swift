ce705ca06271a29395c6af874d0919d34361f0e1
pryme-svg <edoc.www@gmail.com>
2022-02-17 20:06:33 -0800

    global variable demo

---
+++ RapidReactApp2022/RapidReactApp2022/Global.swift
@@ -0,0 +1,14 @@
+//
+//  Global.swift
+//  RapidReactApp2022
+//
+//  Created by William Chen on 2/17/22.
+//
+
+import Foundation
+
+let global: Global = Global()
+
+class Global {
+    var exampleVar: Int = 0
+}
--- RapidReactApp2022/RapidReactApp2022/ViewController.swift
+++ RapidReactApp2022/RapidReactApp2022/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var exampleLabel: UILabel!
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        global.exampleVar = Int(sender.value)
+        exampleLabel.text = global.exampleVar.description
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
