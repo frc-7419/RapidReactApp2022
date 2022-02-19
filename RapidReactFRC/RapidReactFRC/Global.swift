696985efabcbc94b5a1890d535f7951216d08864
AaravWattal <61303690+AaravWattal@users.noreply.github.com>
2022-02-19 15:27:58 -0800

    Add files via upload

---
+++ RapidReactFRC/RapidReactFRC/Global.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+let global: Global = Global()
+
+class Global {
+    var exampleVar: Int = 0
+    var autonArray: [Any] = []
+    var teleOpArray: [Any] = []
+    var endGameArray: [Any] = []
+    var scoutingArray: [Any] = []
+    var problemsData: [[String:Any]] = []
+    var totalArray: [Any] = []
+}
---
+++ RapidReactFRC/RapidReactFRC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RapidReactFRC
+//
+//  Created by Aarav Wattal on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
